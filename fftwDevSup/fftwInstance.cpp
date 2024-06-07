4124422ba9d07dde97580ab74b7b2157a3604a00
Ralph Lange <ralph.lange@gmx.de>
2024-06-07 11:18:17 +0200

    Fix Codacy warnings

--- fftwDevSup/fftwInstance.cpp
+++ fftwDevSup/fftwInstance.cpp
@@ -28,7 +28,7 @@
 #include <minwinbase.h>
 //struct timespec { long tv_sec; long tv_nsec; };    //header part
 int clock_gettime(int, struct timespec *spec)      //C-file part
-{  __int64 wintime; GetSystemTimeAsFileTime((FILETIME*)&wintime);
+{  __int64 wintime; GetSystemTimeAsFileTime(reinterpret_cast<FILETIME*>(&wintime));
     wintime      -=116444736000000000i64;  //1jan1601 to 1jan1970
     spec->tv_sec  =wintime / 10000000i64;           //seconds
     spec->tv_nsec =wintime % 10000000i64 *100;      //nano-seconds
